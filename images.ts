aa10f32dfd6bbd5aa7f32dee784e97262b2e9f5f
itai rozen <itai.rozen@mail.huji.ac.il>
2024-01-14 22:18:21 +0200

    images required in a dedicated file

---
+++ images.ts
@@ -0,0 +1,21 @@
+
+import type { ImageSourcePropType } from "react-native";
+export type imagesType = {
+  '1': ImageSourcePropType,
+  '2': ImageSourcePropType,
+  '3': ImageSourcePropType,
+  '4': ImageSourcePropType,
+  '5': ImageSourcePropType,
+}
+const images : imagesType   = {
+  1 : require('./assets/1.jpg'),
+  2 : require('./assets/2.jpg'),
+  3 : require('./assets/3.jpg'),
+  4 : require('./assets/4.jpg'),
+  5 : require('./assets/5.jpg'),
+};
+export default images
+// 6 : require('./assets/6.jpg'),
+// 7 : require('./assets/7.jpg'),
+// 8 : require('./assets/8.jpg'),
+// 9 : require('./assets/9.jpg'),
