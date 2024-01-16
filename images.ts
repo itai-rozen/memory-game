97060a4ceb3c716bf36f8d52f6595706560551f2
itai rozen <itai.rozen@mail.huji.ac.il>
2024-01-16 19:09:11 +0200

    switched to an array of objects

--- images.ts
+++ images.ts
@@ -1,28 +1,39 @@
 
 import type { ImageSourcePropType } from "react-native";
+
+
 export type imagesType = {
-  '1': ImageSourcePropType,
-  '2': ImageSourcePropType,
-  '3': ImageSourcePropType,
-  '4': ImageSourcePropType,
-  '5': ImageSourcePropType,
-}
-const images : imagesType   = {
-  1 : require('./assets/1.jpg'),
-  2 : require('./assets/2.jpg'),
-  3 : require('./assets/3.jpg'),
-  4 : require('./assets/4.jpg'),
-  5 : require('./assets/5.jpg'),
+  url: ImageSourcePropType,
+  isFlipped: boolean,
+  isMatched: boolean,
+  id?: number
 };
-export const imagesArr  = [
-  require('./assets/1.jpg'),
-  require('./assets/2.jpg'),
-  require('./assets/3.jpg'),
-  require('./assets/4.jpg'),
-  require('./assets/5.jpg'),
-];
-export default images
-// 6 : require('./assets/6.jpg'),
-// 7 : require('./assets/7.jpg'),
-// 8 : require('./assets/8.jpg'),
-// 9 : require('./assets/9.jpg'),
+
+const imagesArr : imagesType[]  = [
+  {
+    url: require('./assets/1.jpg'),
+    isFlipped: false,
+    isMatched: false
+  },
+  {
+    url: require('./assets/2.jpg'),
+    isFlipped: false,
+    isMatched: false
+  },
+  {
+    url: require('./assets/3.jpg'),
+    isFlipped: false,
+    isMatched: false
+  },
+  {
+    url: require('./assets/4.jpg'),
+    isFlipped: false,
+    isMatched: false
+  },
+  {
+    url: require('./assets/5.jpg'),
+    isFlipped: false,
+    isMatched: false
+  },
+]
+export default imagesArr
