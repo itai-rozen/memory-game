efa88212cd26689e8b6d761fb7750305978f40a4
itai rozen <itai.rozen@mail.huji.ac.il>
2024-01-16 19:11:38 +0200

    style changes

--- styles/index.js
+++ styles/index.js
@@ -3,47 +3,61 @@ import { StyleSheet } from "react-native";
 const globals = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'center', 
+    justifyContent: 'center', 
+    margin: 30, 
   },
   CardContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 5,
+    height: 2,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 
   Card: {
-    width: 300,
+    width: 100,
     height: 200,
     borderRadius: 10,
     backgroundColor: 'grey',
-
+    margin: 4,
     // To prevent shadow from overflowing 
     overflow: 'hidden',
   },
   Shadow: {
-    shadowColor: 'red',
+    shadowColor: '#000',
     shadowOpacity: 1,
     shadowRadius: 15,
-    shadowOffset: { width: 0, height: 0 },
+    shadowOffset: { width: 0, height:0 },
   },
   ImageContainer: {
     flex: 1,
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
 
   },
   CardImage: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 4,
+    width: 100,
+    height: 200,
+    // flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
 
   },
   Img : {
-    width: 100,
-    height: 200,
-    resizeMode: 'cover'
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    borderRadius: 4
+  },
+  Cards: {
+    backgroundColor: 'red',
+    flex: 1,
+    flexDirection:'row',
+    justifyContent:'flex-start',
+    flexWrap: 'wrap',
+    width:'100%',
+    height:'100%'
   }
 })
 
