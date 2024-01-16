10997870f7deebf4c73cd6e712c6fbc8a4a40da8
itai rozen <itai.rozen@mail.huji.ac.il>
2024-01-16 19:09:43 +0200

    fixed style bug by moving tyle to touchableopacity

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,33 +1,26 @@
-import { View, Image, Text, TouchableOpacity, ImageSourcePropType } from 'react-native';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
 import { useState } from 'react';
+import { imagesType } from '../images';
 import styles from './../styles';
 import FlipCard from 'react-native-flip-card';
-import images from './../images'
-import type imagesType from './../images'
-export default function Card({imgKey } : { imgKey: ImageSourcePropType} ) {
+export default function Card({ imgObj, setCards }: { imgObj: imagesType, setCards: Function }) {
   const [isFlipped, setIsFlipped] = useState<boolean>(false);
   const handleFlip = () => setIsFlipped(!isFlipped);
-  console.log('image! ', imgKey)
+  // console.log('image! ', imgObj)
   return (
-    <TouchableOpacity onPress={handleFlip}>
-
+    <TouchableOpacity onPress={handleFlip} style={[styles.Card, styles.Shadow]}>
       <FlipCard
-        style={[styles.CardContainer, styles.Shadow]}
         friction={6}
         perspective={1000}
         flipHorizontal={true}
         flipVertical={false}
         flip={isFlipped}
-           >
+      >
         <View style={styles.ImageContainer}>
-          <View style={styles.CardImage}>
-            <Image style={styles.Img}  source={imgKey} />
-          </View>
+          <Image style={styles.Img} source={imgObj.url} />
         </View>
         <View style={styles.ImageContainer}>
-          <View style={styles.CardImage}>
-            <Image  style={styles.Img} source={require('./../assets/card-back.jpg')} />
-          </View>
+          <Image style={styles.Img} source={require('./../assets/card-back.jpg')} />
         </View>
       </FlipCard>
     </TouchableOpacity>
