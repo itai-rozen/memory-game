99e4e787f77504c9de846b0214a6846f1f5c94ff
itai rozen <itai.rozen@mail.huji.ac.il>
2024-01-17 19:40:53 +0200

    working logic without flip animation

--- components/CardTest.tsx
+++ components/CardTest.tsx
@@ -1,25 +1,43 @@
-import { ImageSourcePropType, View, Image, TouchableOpacity } from "react-native";
+import { Animated, View, Image, TouchableOpacity } from "react-native";
 import FlipCard from "react-native-flip-card";
 import styles from './../styles'
+import { imagesType } from "../images";
 
-export default function CardTest({ imgKey }: { imgKey: ImageSourcePropType }) {
-      {/* <FlipCard flipVertical={false} flipHorizontal={true} > */}
-      {/* </FlipCard> */}
-
+export default function CardTest({ cardObj, setCards, cards }: { cardObj: imagesType, cards: imagesType[], setCards: Function }) {
+  {/* <FlipCard flipVertical={false} flipHorizontal={true} > */ }
+  {/* </FlipCard> */ }
+  const handleFlip = () => {
+    if (cardObj.isMatched)
+      return;
+    console.log('fkuo')
+    const cardsCopy = cards.map(card => {return {...card}})
+    const updatedCards = cardsCopy.map(card => {
+      if (card.id === cardObj.id) {
+        card.isFlipped = !card.isFlipped
+      }
+      return card
+    })
+    setCards(updatedCards)
+  }
   return (<>
-    <TouchableOpacity style={[styles.Card, styles.Shadow]}>
-        <View style={styles.ImageContainer}>
-          <Image
-            source={imgKey}
-            style={styles.Img}
-          />
-        </View>
-        <View style={styles.ImageContainer}>
-          <Image
-            source={require('./../assets/card-back.jpg')}
-            style={styles.Img}
-          />
-        </View>
+    <TouchableOpacity onPress={handleFlip} style={[styles.Card, styles.Shadow]}>
+      {
+        cardObj.isFlipped ? <>
+          <View style={styles.ImageContainer}>
+            <Image
+              source={cardObj.url}
+              style={styles.Img}
+            />
+          </View>
+        </> : <>
+          <View style={styles.ImageContainer}>
+            <Image
+              source={require('./../assets/card-back.jpg')}
+              style={styles.Img}
+            />
+          </View>
+        </>
+      }
     </TouchableOpacity>
   </>)
 }
