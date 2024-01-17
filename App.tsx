d2bfaae13db14df805112f2a01b316858fd83116
itai rozen <itai.rozen@mail.huji.ac.il>
2024-01-18 00:53:01 +0200

    added stack navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
-import {  Text, View } from 'react-native';
-import styles from './styles'
+import { Button, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import styles from './styles';
 import Game from './screens/Game';
+import Home from './screens/Home';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Game />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+        headerShown: false
+      }}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Game" component={Game} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
