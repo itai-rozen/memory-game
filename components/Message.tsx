e001752d22c97d644d24a96b87dd615a31086495
itai rozen <itai.rozen@mail.huji.ac.il>
2024-01-17 19:41:26 +0200

    added a button to refresh game if game is over

--- components/Message.tsx
+++ components/Message.tsx
@@ -1,5 +1,10 @@
-import { Text } from "react-native";
+import { Button, Text } from "react-native";
 import styles from './../styles'
-export default function Message({ msg, color }: {msg : string, color: string}) {
-  return <Text style={[styles.Message,{ color: color}]}>{msg}</Text>
+export default function Message({ msg, color, onInit }: {msg : string, color: string, onInit: Function}) {
+  return <>
+    <Text style={[styles.Message,{ color: color}]}>{msg}</Text>
+    {
+      msg === 'again?' && <Button onPress={() => onInit()} title="Yalla" />
+    }
+  </>
 }
