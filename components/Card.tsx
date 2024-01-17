8a7433443413a525c3973788272748cff2da56d5
itai rozen <itai.rozen@mail.huji.ac.il>
2024-01-17 02:23:43 +0200

    on card flip, cards state updated

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,27 +1,35 @@
 import { View, Image, Text, TouchableOpacity } from 'react-native';
-import { useState } from 'react';
 import { imagesType } from '../images';
 import styles from './../styles';
 import FlipCard from 'react-native-flip-card';
-export default function Card({ imgObj, setCards }: { imgObj: imagesType, setCards: Function }) {
-  const [isFlipped, setIsFlipped] = useState<boolean>(false);
-  const handleFlip = () => setIsFlipped(!isFlipped);
-  // console.log('image! ', imgObj)
+export default function Card({ cardObj, setCards, cards }:
+  { cardObj: imagesType, setCards: Function, cards: imagesType[] }) {
+  const handleFlip = () => {
+    console.log('flip!')
+    const updatedCards = cards.map(card => {
+      if (card.id === cardObj.id) {
+        card.isFlipped = !card.isFlipped
+      }
+      return card
+    })
+    setCards(updatedCards)
+  }
   return (
-    <TouchableOpacity onPress={handleFlip} style={[styles.Card, styles.Shadow]}>
+    <TouchableOpacity style={[styles.Card, styles.Shadow]}>
       <FlipCard
         friction={6}
         perspective={1000}
         flipHorizontal={true}
         flipVertical={false}
-        flip={isFlipped}
+        onFlipEnd={handleFlip}
       >
-        <View style={styles.ImageContainer}>
-          <Image style={styles.Img} source={imgObj.url} />
-        </View>
         <View style={styles.ImageContainer}>
           <Image style={styles.Img} source={require('./../assets/card-back.jpg')} />
         </View>
+        <View style={styles.ImageContainer}>
+          <Image style={styles.Img} source={cardObj.url} />
+        </View>
+
       </FlipCard>
     </TouchableOpacity>
 
