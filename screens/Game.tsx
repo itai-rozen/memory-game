b1e8caa6891820ee8b2af75ef041744037e524f6
itai rozen <itai.rozen@mail.huji.ac.il>
2024-01-17 16:16:54 +0200

    created new func to update cads status

--- screens/Game.tsx
+++ screens/Game.tsx
@@ -2,13 +2,14 @@ import Card from "../components/Card";
 import Message from "../components/Message";
 import { View, Text } from "react-native";
 import styles from './../styles'
-import images from './../images'
+import cardObjects from './../images'
 import { useState, useEffect } from "react";
 import type { imagesType } from "./../images";
 export default function Game() {
-  const shuffledCards = images.sort(() => Math.random() - 0.5).map(card => card);
-  const [cards, setCards] = useState<imagesType[]>(shuffledCards)
+
+  const [cards, setCards] = useState<imagesType[]>([])
   const [msg, setMsg] = useState<string>('')
+
 
   const updateCards = (id1: number, id2: number, success: boolean): void => {
     const updatedCards = cards.map(card => {
@@ -23,23 +24,40 @@ export default function Game() {
     setMsg(success ? 'success' : 'wrong');
     setTimeout(() => { setMsg('') }, 2000)
   }
-  useEffect(() => {
-    console.log('cards: ', cards)
 
+  const updateCards2 = (prop : string, success : boolean, id1 : number, id2 : number|null = null) => {
+    const cardIds = id2 ? [id1,id2] : [id1];
+    const updatedCards = cards.map(card => {
+      return cardIds.includes(card.id) ? Object.assign(card, {[prop] : success}) : card
+    }) 
+    setCards(updatedCards)
+  }
+
+  useEffect(() => {
+    if (!cards.length)
+      setCards(cardObjects.sort(() => Math.random() - 0.5))
 
     const allMatched = cards.every(card => card.isMatched)
     if (allMatched) {
       // end game
     }
     const flippedImages = cards.filter(obj => obj.isFlipped && !obj.isMatched);
-    console.log('cards: ', flippedImages)
+    console.log('flipped cards: ', flippedImages)
     if (flippedImages.length > 1) {
       const { id: id1 } = flippedImages[0]
       const { id: id2 } = flippedImages[1]
       if (flippedImages[0].url === flippedImages[1].url) {
-        updateCards(id1 ?? -1, id2 ?? -1, true)
+        updateCards2('isMatched', true, id1, id2)
+        setMsg('success')
+        setTimeout(() => {
+          setMsg('')
+        }, 2000)
       } else {
-
+        setMsg('wrong')
+        setTimeout(() => {
+          updateCards2('isFlipped', false, id1, id2)
+          setMsg('')
+        }, 2000)
       }
     }
   }, [cards])
@@ -52,9 +70,9 @@ export default function Game() {
         ))
       }
       {msg && <View style={styles.MessageContainer}>
-        <Message msg={msg} color='yellow' />
-      </View>
-      }
+          <Message msg={msg} color='yellow' />
+        </View>
+            }
     </View>
 
   </>
