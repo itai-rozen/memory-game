906347d0d4cd793c0b28f513b85a00dc124bc542
itai rozen <itai.rozen@mail.huji.ac.il>
2024-01-18 02:23:02 +0200

    replaced btns with touchable

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,11 +1,16 @@
-import { View, Button } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import styles from './../styles';
+import { NavigationProp, ParamListBase } from '@react-navigation/native';
 
-export default function Home({ navigation } : { navigation: NavigationTimingType}) {
+export default function Home({ navigation }: { navigation: NavigationProp<ParamListBase> }) {
   return (
-  <View style={styles.container}>
-    <Button title="Start Game" onPress={() => navigation.navigate('Game')} />
-    <Button title="Settings" />
-  </View>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.HomeBtn} onPress={() => navigation.navigate('Game')} >
+        <Text style={{fontSize: 24}}>Start Game</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.HomeBtn} onPress={() => navigation.navigate('Settings')} >
+        <Text style={{fontSize: 24}}>Settings</Text>
+      </TouchableOpacity>
+    </View>
   )
 } 
