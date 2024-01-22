e921526ad44c2841460875044884cb886dde4dee
itai rozen <itai.rozen@mail.huji.ac.il>
2024-01-22 20:28:09 +0200

    works without flip animation

--- components/CardTest.tsx
+++ components/CardTest.tsx
@@ -1,16 +1,13 @@
 import { Animated, View, Image, TouchableOpacity } from "react-native";
-import FlipCard from "react-native-flip-card";
 import styles from './../styles'
 import { imagesType } from "../images";
 
 export default function CardTest({ cardObj, setCards, cards }: { cardObj: imagesType, cards: imagesType[], setCards: Function }) {
-  {/* <FlipCard flipVertical={false} flipHorizontal={true} > */ }
-  {/* </FlipCard> */ }
   const handleFlip = () => {
     if (cardObj.isMatched)
       return;
     console.log('fkuo')
-    const cardsCopy = cards.map(card => {return {...card}})
+    const cardsCopy = cards.map(card => { return { ...card } })
     const updatedCards = cardsCopy.map(card => {
       if (card.id === cardObj.id) {
         card.isFlipped = !card.isFlipped
@@ -20,10 +17,10 @@ export default function CardTest({ cardObj, setCards, cards }: { cardObj: images
     setCards(updatedCards)
   }
   return (<>
-    <TouchableOpacity onPress={handleFlip} style={[styles.Card, styles.Shadow]}>
+    <TouchableOpacity style={[styles.Card, styles.Shadow]} onPress={handleFlip}>
       {
         cardObj.isFlipped ? <>
-          <View style={styles.ImageContainer}>
+          <View style={[styles.ImageContainer]}>
             <Image
               source={cardObj.url}
               style={styles.Img}
