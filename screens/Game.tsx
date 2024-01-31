94621bf4cbd8e09d95a1c36cee52b7a7f987154e
itai rozen <itai.rozen@mail.huji.ac.il>
2024-01-31 19:58:17 +0200

    end game checked in sepatate function

--- screens/Game.tsx
+++ screens/Game.tsx
@@ -4,21 +4,25 @@ import { View, SafeAreaView, StatusBar } from "react-native";
 import styles , {AndroidSafeView} from './../styles'
 import { getImages } from './../images'
 import { useState, useEffect } from "react";
-import type { imagesType } from "./../images";
+import type { imagesType, messageType } from "./../types";
 import CardTest from "../components/CardTest";
 import { useCardsPairStore } from "../store";
-export default function Game() {
-  type messageType = 'win'|'success'|'fail'|undefined;
+import { NavigationProp, ParamListBase } from "@react-navigation/native";
+export default function Game({navigation} : {navigation: NavigationProp<ParamListBase>}) {
+
   const [cards, setCards] = useState<imagesType[]>([]);
   const [pressable, setPressabe] = useState<boolean>(true);
   const [msg, setMsg] = useState<messageType>(undefined);
 
   const pairs = useCardsPairStore(state => state.pairs);
 
-  const initGame = () => setCards([]);
+  const initGame = () => {
+    setCards([]);
+    setMsg(undefined);
+  }
 
-  const updateCards = (prop : string, success : boolean, id1 : number, id2 : number|null = null) => {
-    const cardIds = id2 ? [id1,id2] : [id1];
+  const updateCards = (prop : string, success : boolean, id1 : number, id2 : number) => {
+    const cardIds = [id1,id2];
     const updatedCards = cards.map(card => {
       return cardIds.includes(card.id) ? Object.assign(card, {[prop] : success}) : card
     }) 
@@ -32,24 +36,30 @@ export default function Game() {
     }, interval)
   }
 
+  const checkWin = () => cards.length > 0 && cards.every(card => card.isMatched)
+
   useEffect(() => {
+    // start game
     if (!cards.length)
       setCards(getImages(pairs).sort(() => Math.random() - 0.5))
 
-    const allMatched = cards.length && cards.every(card => card.isMatched)
-    if (allMatched) {
+    // end game
+    if (checkWin()) {
       setMsg('win')
       return
-      // end game
     }
     const flippedImages = cards.filter(obj => obj.isFlipped && !obj.isMatched);
     if (flippedImages.length > 1) {
       setPressabe(false)
-      const { id: id1 } = flippedImages[0]
-      const { id: id2 } = flippedImages[1]
-      if (flippedImages[0].url === flippedImages[1].url) {
+      const { id: id1, url: url1 } = flippedImages[0]
+      const { id: id2, url: url2 } = flippedImages[1]
+      if (url1 === url2) {
         updateCards('isMatched', true, id1, id2)
-        popMessage('success', 2000);
+        if (checkWin()) {
+          setMsg('win')
+          return
+        } else 
+          popMessage('success', 2000);
       } else {
         popMessage('fail', 2000)
         setTimeout(() => {
@@ -71,7 +81,7 @@ export default function Game() {
       {
       msg && (
         <View style={styles.MessageContainer}>
-          <Message msg={msg} color='yellow' onInit={initGame} />
+          <Message msg={msg} color='yellow' onInit={initGame} navigation={navigation} />
         </View>
       )}
     </SafeAreaView>
