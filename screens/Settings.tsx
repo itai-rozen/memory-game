f1024d7a5270bea224683527b51832695f097f88
itai rozen <itai.rozen@mail.huji.ac.il>
2024-01-31 19:57:49 +0200

    added option to set number of pairs of cards; stte manage with zustand

--- screens/Settings.tsx
+++ screens/Settings.tsx
@@ -1,30 +1,34 @@
-import { Button, FlatList, Pressable, SectionListComponent, Text, View } from "react-native";
+import {  Pressable,  Text, View } from "react-native";
 import styles from './../styles'
 import { NavigationProp, ParamListBase } from "@react-navigation/native";
 import { useCardsPairStore } from "../store";
-
+import { SelectList } from "react-native-dropdown-select-list";
 
 export default function Settings({ navigation }: { navigation: NavigationProp<ParamListBase>}) {
   const setPairs = useCardsPairStore(state => state.setPairs);
   const pairs = useCardsPairStore(state => state.pairs);
+  const options = [1,2,3,4,5,6,7,8,9,10].map((num) => {
+    return {
+      key: num + '',
+      value: num
+    }
+  })
   return (
     <View style={styles.container}>
-      <Text>how many pairs?</Text>
-      
-      <select name="" id="" value={pairs} onChange={e => setPairs(+e.target.value)}>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-        <option value="8">8</option>
-        <option value="9">9</option>
-        <option value="10">10</option>
-      </select>
-      <Pressable onPress={() => navigation.navigate('Game')}>
-        <Text>Start game</Text>
+      <Text style={{padding: 10}}>How many pairs of cards?</Text>
+      <SelectList
+        boxStyles={{margin: 10, width: '50%'}}
+        dropdownTextStyles={{textAlign: 'right'}}
+        placeholder={'How many pairs?'}
+        data={options} 
+        save='value'
+        search={false}
+        setSelected={(value : number) => setPairs(value)}
+        defaultOption={{key: pairs+'', value: pairs}}
+      />
+
+      <Pressable style={styles.TextBtn} onPress={() => navigation.navigate('Game')}>
+        <Text style={styles.Text}>Start game</Text>
       </Pressable>
     </View>
   )
