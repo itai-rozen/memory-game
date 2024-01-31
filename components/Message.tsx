978ca9cc88614dd145c5b6a84200118ea9d3a642
itai rozen <itai.rozen@mail.huji.ac.il>
2024-01-31 19:56:07 +0200

    added animation and some styles

--- components/Message.tsx
+++ components/Message.tsx
@@ -1,17 +1,35 @@
-import { Button, Text, Image, ImageSourcePropType } from "react-native";
+import {  Text, Image, Animated, Pressable } from "react-native";
 import { gameStatus } from "../images";
+import { messageType } from "../types";
 import styles from './../styles'
-export default function Message({ msg, color, onInit }: {msg : 'win'|'success'|'fail', color: string, onInit: Function}) {
-  return <>
+import { useEffect, useRef } from "react";
+import { NavigationProp, ParamListBase } from "@react-navigation/native";
+export default function Message({ msg, color, onInit, navigation }: {msg : messageType, color: string, onInit: Function, navigation: NavigationProp<ParamListBase>}) {
+ const offsetX = useRef(new Animated.Value(-999)).current;
+ const fadeIn = useRef(new Animated.Value(0)).current;
+
+ useEffect(() => {
+  Animated.timing(offsetX, {toValue: 0, useNativeDriver: true}).start();
+  if (msg !== 'win') {
+    setTimeout(() => {
+      Animated.timing(offsetX, {toValue: 999,  useNativeDriver: true}).start();
+    }, 1500)
+  } else {
+    Animated.timing(fadeIn, {toValue: 1, delay: 500, useNativeDriver: true}).start();
+  }
+ }, [])
+
+ return <Animated.View style={[styles.MessageContainer, {transform: [{translateX: offsetX}]} ]}>
 
     <Text style={[styles.Message,{ color: color}]}>
       {msg}
-      <Image
-        source={gameStatus[msg]}
-      />
     </Text>
+    {msg &&<Image style={ msg === 'win' ? {width: 200, height: 200} : {}} source={gameStatus[msg]} /> }
     {
-      msg === 'win' && <Button onPress={() => onInit()} title="Yalla" />
+      msg === 'win' && <Animated.View style={{opacity: fadeIn}}>
+        <Pressable style={styles.TextBtn} onPress={() => onInit()} ><Text>Play again</Text></Pressable>
+        <Pressable style={styles.TextBtn} onPress={() => navigation.navigate('Home')} ><Text>Exit</Text></Pressable>
+      </Animated.View>
     }
-  </>
+  </Animated.View>
 }
